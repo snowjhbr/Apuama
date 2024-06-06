df9132aafbc910dab1d98b3b53d717ead5a274f1
Isaac Xavier <isaacxavier.wanderson@gmail.com>
2024-06-06 10:55:51 -0300

    exclusão de arquivos que seriam usados caso estivesse com Laravel

--- app/resources/views/home.php
+++
@@ -1,8 +0,0 @@
-<?php $this->layout('master', ['title' => 'User Profile', 'style' => 'home']) ?>
-
-<?php foreach ($itens as $item): ?>
-    <li><?php echo $item; ?></li>
-<?php endforeach; ?>
-<li>Olá</li>
-
-
