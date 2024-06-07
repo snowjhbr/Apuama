63cd17e2e86404e53e66bc8b404b75bd140f96ea
Isaac Xavier <isaacxavier.wanderson@gmail.com>
2024-06-06 23:44:22 -0300

    link

--- contato.php
+++ contato.php
@@ -19,9 +19,9 @@ else{
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
 	<link href="http://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
-	<link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="icon.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="/app/resources/css/default.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="/app/resources/css/fonts.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="/app/resources/css/icon.css" rel="stylesheet" type="text/css" media="all" />
 
 </head>
 
@@ -33,7 +33,7 @@ else{
 					<div id="wrapper1">
 						<div id="espaço_container" class="container">
 							<div id="logo_site">
-								<img src="images/carros_logo.png" alt="some text" width=940 height=200>
+								<img src="/app/resources/images/carros_logo.png" alt="some text" width=940 height=200>
 							</div>
 						</div>
 					</div>
